30a062b65cd4b555d5c12a5d4e17a5f729d2e7ae
jamiekariuki <jamiekariuki18@gmail.com>
2023-10-05 12:24:18 +0300

    icon classname fixed

--- src/components/side bar/links.js
+++ src/components/side bar/links.js
@@ -19,37 +19,37 @@ import LogoutIcon from "@mui/icons-material/Logout";
 //----dashboard
 export const first = [
 	{
-		icon: <PieChartIcon />,
+		icon: <PieChartIcon className="nav-icons" />,
 		text: "Dashboard",
 		link: "/",
 	},
 	{
-		icon: <PeopleIcon />,
+		icon: <PeopleIcon className="nav-icons" />,
 		text: "Users",
 		link: "/users",
 	},
 	{
-		icon: <EqualizerIcon />,
+		icon: <EqualizerIcon className="nav-icons" />,
 		text: "Daily Activity",
 		link: "/daily-activity",
 	},
 	{
-		icon: <BubbleChartIcon />,
+		icon: <BubbleChartIcon className="nav-icons" />,
 		text: "Business insight",
 		link: "/busines-insight",
 	},
 	{
-		icon: <DataSaverOffIcon />,
+		icon: <DataSaverOffIcon className="nav-icons" />,
 		text: "CRM",
 		link: "/crm",
 	},
 	{
-		icon: <PolylineIcon />,
+		icon: <PolylineIcon className="nav-icons" />,
 		text: "Work Flow",
 		link: "/work-flow",
 	},
 	{
-		icon: <InventoryIcon />,
+		icon: <InventoryIcon className="nav-icons" />,
 		text: "Inventory",
 		link: "/inventory",
 	},
@@ -58,18 +58,18 @@ export const first = [
 //----chat bot
 export const chatBot = [
 	{
-		icon: <ViewListIcon />,
+		icon: <ViewListIcon className="nav-icons" />,
 		text: "Overview",
 		link: "/chatbot-overview",
 	},
 
 	{
-		icon: <BuildIcon />,
+		icon: <BuildIcon className="nav-icons" />,
 		text: "Manage",
 		link: "/chatbot-manage",
 	},
 	{
-		icon: <AppRegistrationSharpIcon />,
+		icon: <AppRegistrationSharpIcon className="nav-icons" />,
 		text: "Platforms",
 		link: "/chatbot-platform",
 	},
@@ -107,17 +107,17 @@ export const chatBotPlatform = [
 //-----agents
 export const agents = [
 	{
-		icon: <ViewListIcon />,
+		icon: <ViewListIcon className="nav-icons" />,
 		text: "Overview",
 		link: "/agents-overview",
 	},
 	{
-		icon: <PlaylistAddCheckIcon />,
+		icon: <PlaylistAddCheckIcon className="nav-icons" />,
 		text: "Task Queues",
 		link: "/agents-task-queues",
 	},
 	{
-		icon: <BuildIcon />,
+		icon: <BuildIcon className="nav-icons" />,
 		text: "Manage",
 		link: "/agents-contact",
 	},
@@ -125,22 +125,22 @@ export const agents = [
 //------VPS
 export const vps = [
 	{
-		icon: <SpeedIcon />,
+		icon: <SpeedIcon className="nav-icons" />,
 		text: "Performance ",
 		link: "/vps-performance-metric",
 	},
 	{
-		icon: <StorageIcon />,
+		icon: <StorageIcon className="nav-icons" />,
 		text: "Resource ",
 		link: "/vps-Resource Allocation",
 	},
 	{
-		icon: <FolderCopyIcon />,
+		icon: <FolderCopyIcon className="nav-icons" />,
 		text: "File managment",
 		link: "/vps-file-managment",
 	},
 	{
-		icon: <BuildIcon />,
+		icon: <BuildIcon className="nav-icons" />,
 		text: "manage",
 		link: "/vps-manage",
 	},
@@ -149,17 +149,17 @@ export const vps = [
 //--------general
 export const general = [
 	{
-		icon: <AccountCircleIcon />,
+		icon: <AccountCircleIcon className="nav-icons" />,
 		text: "Profile",
 		link: "/profile",
 	},
 	{
-		icon: <SettingsIcon />,
+		icon: <SettingsIcon className="nav-icons" />,
 		text: "Settings",
 		link: "/settings",
 	},
 	{
-		icon: <LogoutIcon />,
+		icon: <LogoutIcon className="nav-icons" />,
 		text: "Logout",
 		link: "/logout",
 	},
--- src/components/side bar/navigation/navigations.jsx
+++ src/components/side bar/navigation/navigations.jsx
@@ -20,7 +20,6 @@ const Navigations = ({
 							: "side-bar-icon"
 					}
 				>
-					{" "}
 					{icon}
 				</div>
 
