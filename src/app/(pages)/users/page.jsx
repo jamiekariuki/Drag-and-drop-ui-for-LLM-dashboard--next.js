339219fa5b86fc757b3324a4cfb0fa8984412fdb
jamiekariuki <jamiekariuki18@gmail.com>
2023-10-13 18:09:53 +0300

    users added

--- src/app/(pages)/users/page.jsx
+++ src/app/(pages)/users/page.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-//import "./page.user.scss";
-//import UsersOverview from "@/components/pages/users/users overview/users.overview";
-//import UsersTable from "@/components/pages/users/users table/users.table";
+import "./page.user.scss";
+import UsersOverview from "@/components/pages/users/users overview/users.overview";
+import UsersTable from "@/components/pages/users/users table/users.table";
 
 const Users = () => {
 	return (
 		<div className="users">
-			users
-			{/* <div className="users-chart">
+			<div className="users-chart">
 				<UsersOverview />
 			</div>
 			<div className="users-table">
 				<UsersTable />
-			</div> */}
+			</div>
 		</div>
 	);
 };
