e1aa11d4e1f84be17bf823ffbc857a5c160ddda3
jamiekariuki <jamiekariuki18@gmail.com>
2023-11-17 11:59:15 +0300

    mui modal error fixed

--- src/components/styled components/modals/modal.jsx
+++ src/components/styled components/modals/modal.jsx
@@ -99,10 +99,7 @@ Modal.propTypes = {
 
 export default Modal; */
 
-import * as React from "react";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 
 const style = {
