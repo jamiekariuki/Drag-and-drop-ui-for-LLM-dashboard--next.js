9b53d178e7b243214f9fd21a06037593db74db75
jamiekariuki <jamiekariuki18@gmail.com>
2023-11-17 11:56:14 +0300

    mui modal

--- src/components/styled components/modals/modal.jsx
+++ src/components/styled components/modals/modal.jsx
@@ -5,7 +5,8 @@ import PropTypes from "prop-types";
 import CloseIcon from "@mui/icons-material/Close";
 import IconButton from "@mui/material/IconButton";
 
-/* const Modal = ({ open, onClose, children }) => {
+/*
+ const Modal = ({ open, onClose, children }) => {
 	const ref = useRef(null);
 
 	useLayoutEffect(() => {
@@ -67,7 +68,7 @@ Modal.propTypes = {
 
 export default Modal;
  */
-
+/* 
 const Modal = ({ open, onClose, children }) => {
 	if (open) {
 		return (
@@ -96,4 +97,52 @@ Modal.propTypes = {
 	children: PropTypes.node.isRequired,
 };
 
-export default Modal;
+export default Modal; */
+
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+
+const style = {
+	position: "absolute",
+	top: "50%",
+	left: "50%",
+	transform: "translate(-50%, -50%)",
+	width: 400,
+	bgcolor: "background.paper",
+	border: "2px solid #000",
+	boxShadow: 24,
+	p: 4,
+};
+
+export default function Modal({ open, onClose, children }) {
+	/* const [open, setOpen] = React.useState(false);
+	const handleOpen = () => setOpen(true);
+	const handleClose = () => setOpen(false); */
+
+	return (
+		<div>
+			{/* <Button onClick={handleOpen}>Open modal</Button> */}
+			<Modal
+				open={open}
+				onClose={onClose}
+				aria-labelledby="modal-modal-title"
+				aria-describedby="modal-modal-description"
+			>
+				<Box /* sx={style} */ className="modal ">
+					<IconButton
+						aria-label="close"
+						size="medium"
+						className="close-button"
+						onClick={onClose}
+					>
+						<CloseIcon fontSize="inherit" />
+					</IconButton>
+					{children}
+				</Box>
+			</Modal>
+		</div>
+	);
+}
