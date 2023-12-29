85abcfd0ef657fee795dfdc038efbe895eb8b25c
jamiekariuki <jamiekariuki18@gmail.com>
2023-12-29 16:19:39 +0300

    date update 2

--- src/app/(pages)/agents/[id]/page.jsx
+++ src/app/(pages)/agents/[id]/page.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "./page.workflow.scss";
-//import Workflow from "@/components/pages/agents/work Flow/workflow";
+import Workflow from "@/components/pages/agents/work Flow/workflow";
 
 const WorkFlowPage = () => {
-	return <div className="workflow-page">{/* <Workflow /> */}</div>;
+	return (
+		<div className="workflow-page">
+			{" "}
+			<Workflow />{" "}
+		</div>
+	);
 };
 
 export default WorkFlowPage;
