a7d1d253770651487462c5f8f8b472ef7d77980a
jamiekariuki <jamiekariuki18@gmail.com>
2024-01-05 12:44:54 +0300

    agents page test

--- src/app/(pages)/agents/page.jsx
+++ src/app/(pages)/agents/page.jsx
@@ -1,11 +1,12 @@
-import React from "react";
 import PageContainer from "@/components/styled components/containers/page container/page.container";
 import Agents from "@/components/pages/agents/agents/agents";
 
-const Agentspage = () => {
-	<PageContainer>
-		<Agents />
-	</PageContainer>;
+const AgentsPage = () => {
+	return (
+		<PageContainer>
+			<Agents />
+		</PageContainer>
+	);
 };
 
-export default Agentspage;
+export default AgentsPage;
