0f20a9e3efcbf65d3a9b684f1bb3a3840d455ce1
jamiekariuki <jamiekariuki18@gmail.com>
2024-01-05 12:02:15 +0300

    dashboard test

--- src/app/(pages)/dashboard/page.jsx
+++ src/app/(pages)/dashboard/page.jsx
@@ -1,8 +1,13 @@
-//import Dashboard from "@/components/pages/dashboard/dashboard";
+import Dashboard from "@/components/pages/dashboard/dashboard";
 import PageContainer from "@/components/styled components/containers/page container/page.container";
 
 const DashboardPage = () => {
-	return <PageContainer>{/* <Dashboard /> */}</PageContainer>;
+	return (
+		<PageContainer>
+			{" "}
+			<Dashboard />{" "}
+		</PageContainer>
+	);
 };
 
 export default DashboardPage;
